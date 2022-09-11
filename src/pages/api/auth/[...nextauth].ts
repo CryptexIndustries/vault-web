1cf29d1f70d2b8417d3c836ada628ca152f65569
SyStem <g1-assassin@hotmail.com>
2022-09-11 22:07:08 +0200

    Initial commit

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,60 @@
+import NextAuth, { type NextAuthOptions } from "next-auth";
+
+import GoogleProvider from "next-auth/providers/google";
+import AppleProvider from "next-auth/providers/apple";
+import GithubProvider from "next-auth/providers/github";
+import GitlabProvider from "next-auth/providers/gitlab";
+import AtlassianProvider from "next-auth/providers/atlassian";
+import DiscordProvider from "next-auth/providers/discord";
+
+// Prisma adapter for NextAuth, optional and can be removed
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { prisma } from "../../../server/db/client";
+import { env } from "../../../env/server.mjs";
+
+export const authOptions: NextAuthOptions = {
+    // Include user.id on session
+    callbacks: {
+        session({ session, user }) {
+            if (session.user) {
+                session.user.id = user.id;
+            }
+            return session;
+        },
+    },
+    secret: env.NEXTAUTH_SECRET,
+    session: {
+        strategy: "database",
+        maxAge: 30 * 24 * 60 * 60, // 30 days
+    },
+    // Configure one or more authentication providers
+    adapter: PrismaAdapter(prisma),
+    providers: [
+        GoogleProvider({
+            clientId: env.GOOGLE_CLIENT_ID,
+            clientSecret: env.GOOGLE_CLIENT_SECRET,
+        }),
+        AppleProvider({
+            clientId: env.APPLE_CLIENT_ID,
+            clientSecret: env.APPLE_CLIENT_SECRET,
+        }),
+        GithubProvider({
+            clientId: env.GITHUB_CLIENT_ID,
+            clientSecret: env.GITHUB_CLIENT_SECRET,
+        }),
+        GitlabProvider({
+            clientId: env.GITLAB_CLIENT_ID,
+            clientSecret: env.GITLAB_CLIENT_SECRET,
+        }),
+        AtlassianProvider({
+            clientId: env.ATLASSIAN_CLIENT_ID,
+            clientSecret: env.ATLASSIAN_CLIENT_SECRET,
+        }),
+        DiscordProvider({
+            clientId: env.DISCORD_CLIENT_ID,
+            clientSecret: env.DISCORD_CLIENT_SECRET,
+        }),
+    ],
+};
+
+export default NextAuth(authOptions);

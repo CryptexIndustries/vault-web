8be988147e0eb0487f59b51930469482cd7c1db0
SyStem <g1-assassin@hotmail.com>
2022-09-26 00:41:16 +0200

    Index page WIP

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -78,33 +78,62 @@ const Index: NextPage = ({}) => {
                     <section id="section-about" className="h-screen">
                         <div className="h-full flex items-center justify-evenly">
                             <div className="max-w-lg">
-                                <h1 className="text-3xl">
-                                    We started as a drajen mušketiren wit no
-                                    mani.We started as a drajen mušketiren wit
-                                    no mani.We started as a drajen mušketiren
-                                    wit no mani.We started as a drajen
-                                    mušketiren wit no mani.We started as a
-                                    drajen mušketiren wit no mani.We started as
-                                    a drajen mušketiren wit no mani.We started
-                                    as a drajen mušketiren wit no mani.We
-                                    started as a drajen mušketiren wit no
-                                    mani.We started as a drajen mušketiren wit
-                                    no mani.
+                                <h1 className="text-5xl text-gray-300">
+                                    What is{" "}
+                                    <b className="text-rose-400">
+                                        Cryptex Vault{" "}
+                                    </b>
+                                    and why should you use it? <br />
                                 </h1>
                             </div>
-
+                            {/* Roses are red, violets are blue, we're here
+                                    to protect you. */}
                             <div className="max-w-lg">
-                                <p>
-                                    MIHA JE
-                                    GEYYYaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaJE
-                                    GEYYYaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaJE
-                                    GEYYYaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaJE
-                                    GEYYYaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaJE
-                                    GEYYYaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaJE
-                                    GEYYYaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaJE
-                                    GEYYYaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaJE
-                                    GEYYYaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+                                <p className="text-gray-500 mb-2">
+                                    What are we?
                                 </p>
+                                <p className="ml-4 text-gray-300">
+                                    Cryptex Vault is a fully decentralized
+                                    identity manager. It allows you to store
+                                    your credentials, secrets and other
+                                    information in a secure way using mobile
+                                    applications and browser extensions.
+                                </p>
+                                <p className="text-gray-500 mt-4 mb-2">
+                                    How the rest of the industry works?
+                                </p>
+                                <p className="ml-4 text-gray-300">
+                                    Most of the password managers out there are
+                                    centralized. They store your data on their
+                                    servers, which means that they can read it.
+                                    They can also sell your data to third
+                                    parties. We don't want that. We want to give
+                                    you the power to control your data.
+                                </p>
+                                <p className="text-gray-500 mt-4 mb-2">
+                                    How we do it.
+                                </p>
+                                <p className="ml-3 text-gray-300">
+                                    Unlike other password managers, Cryptex
+                                    Vault doesn't store your data. It's only
+                                    stored on your devices, not servers.
+                                    <br /> You can use it on any supported
+                                    platform.
+                                </p>
+
+                                <p className="text-gray-500  mt-10 mb-2">
+                                    Available platforms
+                                </p>
+                                <div className="flex">
+                                    <a href="https://play.google.com/store/apps/details?id=com.lastpass.lpandroid&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1">
+                                        <img
+                                            width={200}
+                                            height={80}
+                                            alt="Get it on Google Play"
+                                            src="https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png"
+                                        />
+                                    </a>
+                                </div>
                             </div>
                         </div>
                     </section>
@@ -112,8 +141,10 @@ const Index: NextPage = ({}) => {
                     <section id="section-pricing" className="h-full">
                         <div className="flex w-full justify-center">
                             <h1 className="text-7xl leading-snug">
-                                Pricing for all types of <br />{" "}
-                                <span className="text-rose-400">companies</span>{" "}
+                                Pricing for all types of <br />
+                                <span className="text-rose-400">
+                                    companies
+                                </span>{" "}
                                 and <span className="text-rose-400">needs</span>
                             </h1>
                         </div>

08868a4e6c1ebedb5763189335e9fde9e85399bd
SyStem <g1-assassin@hotmail.com>
2022-10-01 18:16:28 +0200

    Index page cleanup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -135,12 +135,6 @@ const Index: NextPage = ({}) => {
                                 </p>
                                 <div className="flex items-center">
                                     <a href="https://play.google.com/store/apps/details?id=com.lastpass.lpandroid&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1">
-                                        {/* <img
-                                            width={200}
-                                            height={80}
-                                            alt="Get it on Google Play"
-                                            src="https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png"
-                                        /> */}
                                         <Image
                                             width={200}
                                             height={80}

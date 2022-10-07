c0fbf5c8bc1320d8ba83551eabbf131ed33e4409
SyStem <g1-assassin@hotmail.com>
2022-10-07 21:24:32 +0200

    Removed/disabled dangerous routes - for this pre-release

--- src/server/trpc/index.ts
+++ src/server/trpc/index.ts
@@ -2,14 +2,14 @@
 import { createRouter } from "./context";
 import superjson from "superjson";
 
-import { protectedExampleRouter } from "./routes/protected-example-router";
-import { credentialsRouter } from "./routes/credentials.router";
+// import { protectedExampleRouter } from "./routes/protected-example-router";
+// import { credentialsRouter } from "./routes/credentials.router";
 import { notifyMeRouter } from "./routes/notifyme.router";
 export const appRouter = createRouter()
     .transformer(superjson)
-    .merge("notifyme.", notifyMeRouter)
-    .merge("credentials.", credentialsRouter)
-    .merge("auth.", protectedExampleRouter);
+    .merge("notifyme.", notifyMeRouter);
+// .merge("credentials.", credentialsRouter)
+// .merge("auth.", protectedExampleRouter);
 
 // export type definition of API
 export type AppRouter = typeof appRouter;

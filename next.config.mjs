c73a8812e9627d6a4da6cae94253ec905fedf3ca
SyStem <g1-assassin@hotmail.com>
2022-10-07 23:11:07 +0200

    Fixed security headers, I think...

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,40 @@ function defineNextConfig(config) {
     return config;
 }
 
+const headers = () => {
+    return [
+        {
+            source: "/(.*)",
+            headers: [
+                {
+                    key: "X-DNS-Prefetch-Control",
+                    value: "on",
+                },
+                {
+                    key: "X-Frame-Options",
+                    value: "DENY",
+                },
+                {
+                    key: "X-XSS-Protection",
+                    value: "1; mode=block",
+                },
+                {
+                    key: "X-Content-Type-Options",
+                    value: "nosniff",
+                },
+                {
+                    key: "Referrer-Policy",
+                    value: "strict-origin-when-cross-origin",
+                },
+                {
+                    key: "Permissions-Policy",
+                    value: "camera=(), microphone=(), geolocation=(), interest-cohort=()",
+                },
+            ],
+        },
+    ];
+};
+
 const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
@@ -21,32 +55,7 @@ const nextConfig = {
     typescript: {
         ignoreBuildErrors: true,
     },
-    headers: async () => [
-        {
-            key: "X-DNS-Prefetch-Control",
-            value: "on",
-        },
-        {
-            key: "X-Frame-Options",
-            value: "DENY",
-        },
-        {
-            key: "X-XSS-Protection",
-            value: "1; mode=block",
-        },
-        {
-            key: "X-Content-Type-Options",
-            value: "nosniff",
-        },
-        {
-            key: "Referrer-Policy",
-            value: "strict-origin-when-cross-origin",
-        },
-        {
-            key: "Permissions-Policy",
-            value: "camera=(), microphone=(), geolocation=(), interest-cohort=()",
-        },
-    ],
+    headers,
 };
 
 // const withBundleAnalyzer = require("@next/bundle-analyzer")({

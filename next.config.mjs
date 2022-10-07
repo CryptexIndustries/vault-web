7ecb6f720f80acebfb5f56b0a5104e3dc0ad145d
SyStem <g1-assassin@hotmail.com>
2022-10-07 21:23:24 +0200

    Added security headers

--- next.config.mjs
+++ next.config.mjs
@@ -21,6 +21,32 @@ const nextConfig = {
     typescript: {
         ignoreBuildErrors: true,
     },
+    headers: [
+        {
+            key: "X-DNS-Prefetch-Control",
+            value: "on",
+        },
+        {
+            key: "X-Frame-Options",
+            value: "DENY",
+        },
+        {
+            key: "X-XSS-Protection",
+            value: "1; mode=block",
+        },
+        {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+        },
+        {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+        },
+        {
+            key: "Permissions-Policy",
+            value: "camera=(), microphone=(), geolocation=(), interest-cohort=()",
+        },
+    ],
 };
 
 // const withBundleAnalyzer = require("@next/bundle-analyzer")({

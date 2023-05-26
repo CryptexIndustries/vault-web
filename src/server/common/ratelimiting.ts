e3907e90fb656f6cf4130ab21324e4d18961f522
Bl4cksmith <10003460+SyStem-5@users.noreply.github.com>
2023-05-27 00:03:17 +0200

    Cleaned up ratelimiting, scoped the ratelimit object instantiation

--- src/server/common/ratelimiting.ts
+++ src/server/common/ratelimiting.ts
@@ -10,22 +10,24 @@ type Duration = `${number} ${Unit}` | `${number}${Unit}`;
 const ephemeralCache = new Map();
 
 //#region Auth
-const ratelimitAuth: Ratelimit = new Ratelimit({
-    redis: Redis.fromEnv(),
-    limiter: Ratelimit.slidingWindow(
-        env.UPSTASH_RATELIMIT_N_REQUESTS_AUTH,
-        env.UPSTASH_RATELIMIT_DURATION_AUTH as Duration
-    ),
-    analytics: true,
-    ephemeralCache,
-    prefix: "@upstash/ratelimit/auth",
-});
 /**
  * Ratelimit for all requests
- * Rate limit is 2 requests per 5 seconds
+ * @param ip IP address of the user
+ * @returns Whether the request is allowed or not
  */
 export const checkAuthRatelimit = async (ip: string) => {
     if (!env.UPSTASH_RATELIMIT_ENABLE_AUTH) return true;
+
+    const ratelimitAuth: Ratelimit = new Ratelimit({
+        redis: Redis.fromEnv(),
+        limiter: Ratelimit.slidingWindow(
+            env.UPSTASH_RATELIMIT_N_REQUESTS_AUTH,
+            env.UPSTASH_RATELIMIT_DURATION_AUTH as Duration
+        ),
+        analytics: true,
+        ephemeralCache,
+        prefix: "@upstash/ratelimit/auth",
+    });
 
     const result = await ratelimitAuth.limit(ip);
     return result.success;
@@ -33,22 +35,24 @@ export const checkAuthRatelimit = async (ip: string) => {
 //#endregion Auth
 
 //#region Register User
-const ratelimitRegisterUser: Ratelimit = new Ratelimit({
-    redis: Redis.fromEnv(),
-    limiter: Ratelimit.slidingWindow(
-        env.UPSTASH_RATELIMIT_N_REQUESTS_REGISTER,
-        env.UPSTASH_RATELIMIT_DURATION_REGISTER as Duration
-    ),
-    analytics: true,
-    ephemeralCache,
-    prefix: "@upstash/ratelimit/register-user",
-});
 /**
  * Ratelimit for registering a new user
- * Rate limit is 1 request per minute
+ * @param ip IP address of the user
+ * @returns Whether the request is allowed or not
  */
 export const checkRatelimitRegisterUser = async (ip: string) => {
     if (!env.UPSTASH_RATELIMIT_ENABLE_REGISTER) return true;
+
+    const ratelimitRegisterUser: Ratelimit = new Ratelimit({
+        redis: Redis.fromEnv(),
+        limiter: Ratelimit.slidingWindow(
+            env.UPSTASH_RATELIMIT_N_REQUESTS_REGISTER,
+            env.UPSTASH_RATELIMIT_DURATION_REGISTER as Duration
+        ),
+        analytics: true,
+        ephemeralCache,
+        prefix: "@upstash/ratelimit/register-user",
+    });
 
     const result = await ratelimitRegisterUser.limit(ip);
     return result.success;
@@ -56,30 +60,8 @@ export const checkRatelimitRegisterUser = async (ip: string) => {
 //#endregion Register User
 
 //#region PaymentRouter
-const ratelimitPaymentRouter: Ratelimit = new Ratelimit({
-    redis: Redis.fromEnv(),
-    limiter: Ratelimit.slidingWindow(
-        env.UPSTASH_RATELIMIT_N_REQUESTS_PAYMENTROUTER_DB,
-        env.UPSTASH_RATELIMIT_DURATION_PAYMENTROUTER_DB as Duration
-    ),
-    analytics: true,
-    ephemeralCache,
-    prefix: "@upstash/ratelimit/payment-router",
-});
-const ratelimitPaymentNoDB: Ratelimit = new Ratelimit({
-    redis: Redis.fromEnv(),
-    limiter: Ratelimit.slidingWindow(
-        env.UPSTASH_RATELIMIT_N_REQUESTS_PAYMENTROUTER_NODB,
-        env.UPSTASH_RATELIMIT_DURATION_PAYMENTROUTER_NODB as Duration
-    ),
-    analytics: true,
-    ephemeralCache,
-    prefix: "@upstash/ratelimit/payment-router-no-db",
-});
 /**
  * Ratelimit for payment router
- * Rate limit is 3 requests per 5 seconds for requests that use a database
- * Rate limit is 3 requests per 5 seconds for requests that don't use a database
  * @param ip IP address of the user
  * @param usesDB Whether the user uses a database or not
  * @returns Whether the request is allowed or not
@@ -94,39 +76,43 @@ export const checkRatelimitPaymentRouter = async (
     )
         return true;
 
-    const result = await (usesDB
-        ? ratelimitPaymentRouter
-        : ratelimitPaymentNoDB
-    ).limit(ip);
-    return result.success;
+    let result = false;
+
+    if (usesDB) {
+        const ratelimitPaymentRouter: Ratelimit = new Ratelimit({
+            redis: Redis.fromEnv(),
+            limiter: Ratelimit.slidingWindow(
+                env.UPSTASH_RATELIMIT_N_REQUESTS_PAYMENTROUTER_DB,
+                env.UPSTASH_RATELIMIT_DURATION_PAYMENTROUTER_DB as Duration
+            ),
+            analytics: true,
+            ephemeralCache,
+            prefix: "@upstash/ratelimit/payment-router",
+        });
+
+        result = (await ratelimitPaymentRouter.limit(ip)).success;
+    } else {
+        const ratelimitPaymentNoDB: Ratelimit = new Ratelimit({
+            redis: Redis.fromEnv(),
+            limiter: Ratelimit.slidingWindow(
+                env.UPSTASH_RATELIMIT_N_REQUESTS_PAYMENTROUTER_NODB,
+                env.UPSTASH_RATELIMIT_DURATION_PAYMENTROUTER_NODB as Duration
+            ),
+            analytics: true,
+            ephemeralCache,
+            prefix: "@upstash/ratelimit/payment-router-no-db",
+        });
+
+        result = (await ratelimitPaymentNoDB.limit(ip)).success;
+    }
+
+    return result;
 };
 //#endregion PaymentRouter
 
 //#region Feature Voting
-const ratelimitFeatureVoting: Ratelimit = new Ratelimit({
-    redis: Redis.fromEnv(),
-    limiter: Ratelimit.slidingWindow(
-        env.UPSTASH_RATELIMIT_N_REQUESTS_FEATUREVOTINGROUTER,
-        env.UPSTASH_RATELIMIT_DURATION_FEATUREVOTINGROUTER as Duration
-    ),
-    analytics: true,
-    ephemeralCache,
-    prefix: "@upstash/ratelimit/feature-voting",
-});
-const ratelimitFeatureVotingMutation: Ratelimit = new Ratelimit({
-    redis: Redis.fromEnv(),
-    limiter: Ratelimit.slidingWindow(
-        env.UPSTASH_RATELIMIT_N_REQUESTS_FEATUREVOTINGROUTER_MUTATION,
-        env.UPSTASH_RATELIMIT_DURATION_FEATUREVOTINGROUTER_MUTATION as Duration
-    ),
-    analytics: true,
-    ephemeralCache,
-    prefix: "@upstash/ratelimit/feature-voting-mutation",
-});
 /**
  * Ratelimit for feature voting
- * Rate limit is 3 requests per 10 seconds for queries
- * Rate limit is 1 request per minute for mutations
  * @param ip IP address of the user
  * @param isMutation Whether the request is a mutation or not
  * @returns Whether the request is allowed or not
@@ -142,31 +128,59 @@ export const checkRatelimitFeatureVoting = async (
     )
         return true;
 
-    const result = await (isMutation
-        ? ratelimitFeatureVotingMutation
-        : ratelimitFeatureVoting
-    ).limit(ip);
-    return result.success;
+    let result = false;
+
+    if (isMutation) {
+        const ratelimitFeatureVotingMutation: Ratelimit = new Ratelimit({
+            redis: Redis.fromEnv(),
+            limiter: Ratelimit.slidingWindow(
+                env.UPSTASH_RATELIMIT_N_REQUESTS_FEATUREVOTINGROUTER_MUTATION,
+                env.UPSTASH_RATELIMIT_DURATION_FEATUREVOTINGROUTER_MUTATION as Duration
+            ),
+            analytics: true,
+            ephemeralCache,
+            prefix: "@upstash/ratelimit/feature-voting-mutation",
+        });
+
+        result = (await ratelimitFeatureVotingMutation.limit(ip)).success;
+    } else {
+        const ratelimitFeatureVoting: Ratelimit = new Ratelimit({
+            redis: Redis.fromEnv(),
+            limiter: Ratelimit.slidingWindow(
+                env.UPSTASH_RATELIMIT_N_REQUESTS_FEATUREVOTINGROUTER,
+                env.UPSTASH_RATELIMIT_DURATION_FEATUREVOTINGROUTER as Duration
+            ),
+            analytics: true,
+            ephemeralCache,
+            prefix: "@upstash/ratelimit/feature-voting",
+        });
+
+        result = (await ratelimitFeatureVoting.limit(ip)).success;
+    }
+
+    return result;
 };
 //#endregion Feature Voting
 
 //#region Notifyme
-const ratelimitNotifymeRouter: Ratelimit = new Ratelimit({
-    redis: Redis.fromEnv(),
-    limiter: Ratelimit.slidingWindow(
-        env.UPSTASH_RATELIMIT_N_REQUESTS_NOTIFYMEROUTER,
-        env.UPSTASH_RATELIMIT_DURATION_NOTIFYMEROUTER as Duration
-    ),
-    analytics: true,
-    ephemeralCache,
-    prefix: "@upstash/ratelimit/notifyme",
-});
 /**
  * Ratelimit for notifyme router requests
- * Rate limit is 5 requests per minute
+ * @param ip IP address of the user
+ * @returns Whether the request is allowed or not
  */
 export const checkRatelimitNotifyme = async (ip: string) => {
     if (!env.UPSTASH_RATELIMIT_ENABLE_NOTIFYMEROUTER) return true;
+
+    const ratelimitNotifymeRouter: Ratelimit = new Ratelimit({
+        redis: Redis.fromEnv(),
+        limiter: Ratelimit.slidingWindow(
+            env.UPSTASH_RATELIMIT_N_REQUESTS_NOTIFYMEROUTER,
+            env.UPSTASH_RATELIMIT_DURATION_NOTIFYMEROUTER as Duration
+        ),
+        analytics: true,
+        ephemeralCache,
+        prefix: "@upstash/ratelimit/notifyme",
+    });
 
     const result = await ratelimitNotifymeRouter.limit(ip);
     return result.success;
@@ -174,30 +188,8 @@ export const checkRatelimitNotifyme = async (ip: string) => {
 //#endregion Notifyme
 
 //#region AccountRouter
-const ratelimitAccountRouterQuery: Ratelimit = new Ratelimit({
-    redis: Redis.fromEnv(),
-    limiter: Ratelimit.slidingWindow(
-        env.UPSTASH_RATELIMIT_N_REQUESTS_ACCOUNTROUTER_QUERY,
-        env.UPSTASH_RATELIMIT_DURATION_ACCOUNTROUTER_QUERY as Duration
-    ),
-    analytics: true,
-    ephemeralCache,
-    prefix: "@upstash/ratelimit/account-router-query",
-});
-const ratelimitAccountRouterMutation: Ratelimit = new Ratelimit({
-    redis: Redis.fromEnv(),
-    limiter: Ratelimit.slidingWindow(
-        env.UPSTASH_RATELIMIT_N_REQUESTS_ACCOUNTROUTER_MUTATION,
-        env.UPSTASH_RATELIMIT_DURATION_ACCOUNTROUTER_MUTATION as Duration
-    ),
-    analytics: true,
-    ephemeralCache,
-    prefix: "@upstash/ratelimit/account-router-mutation",
-});
 /**
  * Ratelimit for account router requests
- * Rate limit is 4 requests per 10 seconds for queries
- * Rate limit is 5 requests per 5 seconds for mutations
  * @param ip IP address of the user
  * @param isMutation Whether the request is a mutation or not
  * @returns Whether the request is allowed or not
@@ -212,34 +204,60 @@ export const checkRatelimitAccountRouter = async (
     )
         return true;
 
-    const result = await (isMutation
-        ? ratelimitAccountRouterMutation
-        : ratelimitAccountRouterQuery
-    ).limit(ip);
-    return result.success;
+    let result = false;
+
+    if (isMutation) {
+        const ratelimitAccountRouterMutation: Ratelimit = new Ratelimit({
+            redis: Redis.fromEnv(),
+            limiter: Ratelimit.slidingWindow(
+                env.UPSTASH_RATELIMIT_N_REQUESTS_ACCOUNTROUTER_MUTATION,
+                env.UPSTASH_RATELIMIT_DURATION_ACCOUNTROUTER_MUTATION as Duration
+            ),
+            analytics: true,
+            ephemeralCache,
+            prefix: "@upstash/ratelimit/account-router-mutation",
+        });
+
+        result = (await ratelimitAccountRouterMutation.limit(ip)).success;
+    } else {
+        const ratelimitAccountRouterQuery: Ratelimit = new Ratelimit({
+            redis: Redis.fromEnv(),
+            limiter: Ratelimit.slidingWindow(
+                env.UPSTASH_RATELIMIT_N_REQUESTS_ACCOUNTROUTER_QUERY,
+                env.UPSTASH_RATELIMIT_DURATION_ACCOUNTROUTER_QUERY as Duration
+            ),
+            analytics: true,
+            ephemeralCache,
+            prefix: "@upstash/ratelimit/account-router-query",
+        });
+
+        result = (await ratelimitAccountRouterQuery.limit(ip)).success;
+    }
+
+    return result;
 };
 
 //#endregion AccountRouter
 
 //#region API - Pusher
-const ratelimitPusherRouter: Ratelimit = new Ratelimit({
-    redis: Redis.fromEnv(),
-    limiter: Ratelimit.slidingWindow(
-        env.UPSTASH_RATELIMIT_N_REQUESTS_API_PUSHER,
-        env.UPSTASH_RATELIMIT_DURATION_API_PUSHER as Duration
-    ),
-    analytics: true,
-    ephemeralCache,
-    prefix: "@upstash/ratelimit/pusher",
-});
 /**
  * Ratelimit for pusher router requests
- * Rate limit is 5 requests per 30 seconds
  * @param ip IP address of the user
  * @returns Whether the request is allowed or not
  */
 export const checkRatelimitPusher = async (ip: string) => {
     if (!env.UPSTASH_RATELIMIT_ENABLE_API_PUSHER) return true;
+
+    const ratelimitPusherRouter: Ratelimit = new Ratelimit({
+        redis: Redis.fromEnv(),
+        limiter: Ratelimit.slidingWindow(
+            env.UPSTASH_RATELIMIT_N_REQUESTS_API_PUSHER,
+            env.UPSTASH_RATELIMIT_DURATION_API_PUSHER as Duration
+        ),
+        analytics: true,
+        ephemeralCache,
+        prefix: "@upstash/ratelimit/pusher",
+    });
 
     const result = await ratelimitPusherRouter.limit(ip);
     return result.success;

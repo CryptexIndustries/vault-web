67b673d46f685e656f144ea400d1f59770181a99
Bl4cksmith <10003460+SyStem-5@users.noreply.github.com>
2023-05-27 19:19:06 +0200

    Improved the captcha method documentation and added a proper return type

--- src/utils/captcha.ts
+++ src/utils/captcha.ts
@@ -1,4 +1,48 @@
-export default async function validateCaptcha(token: string) {
+// Type that gets returned when the "success" property is true
+/*
+{
+  "success": true,
+  "challenge_ts": "2022-02-28T15:14:30.096Z",
+  "hostname": "example.com",
+  "error-codes": [],
+  "action": "login",
+  "cdata": "sessionid-123456789"
+}
+*/
+
+// Type that gets returned when the "success" property is false
+/*
+{
+  "success": false,
+  "error-codes": [
+    "invalid-input-response"
+  ]
+}
+*/
+
+type CaptchaResponse = {
+    success: boolean;
+
+    // These properties are only present when success is true
+    challenge_ts?: string;
+    hostname?: string;
+    action?: string;
+    cdata?: string;
+
+    // These properties are only present when success is false
+    "error-codes"?: string[];
+};
+
+/**
+ * Validate a captcha token
+ * @param token Captcha token
+ * @returns Captcha response
+ * @throws Error if TURNSTILE_SECRET is not defined
+ * @throws Error if the call to the captcha API fails
+ */
+export default async function validateCaptcha(
+    token: string
+): Promise<CaptchaResponse> {
     if (!process.env.TURNSTILE_SECRET) {
         throw new Error("TURNSTILE_SECRET is not defined");
     }

4b7c425206ddb5f4384ed1f876f70ebe12fe3568
Bl4cksmith <10003460+SyStem-5@users.noreply.github.com>
2023-07-27 20:40:47 +0200

    improvement: Accordion component will be more obedient when the innerClassName prop is set from now on

--- src/components/general/accordion.tsx
+++ src/components/general/accordion.tsx
@@ -57,6 +57,7 @@ export const AccordionItem: React.FC<AccordionItemProps> = ({
                     leave="transition duration-75 ease-out"
                     leaveFrom="transform scale-100 opacity-100"
                     leaveTo="transform scale-95 opacity-0"
+                    className={`${innerClassName}`}
                 >
                     <Disclosure.Panel className={`${innerClassName}`}>
                         {children}
